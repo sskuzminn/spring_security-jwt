aa2350402046caae8c67e27ea4d5e5bad2c53b94
Sergey Kuzmin <s.s.kuzmin70@yandex.ru>
2024-03-30 03:08:36 +0700

    adding "/authenticate" controller, passwordEncoder

--- src/main/java/com/example/SpringSecurityJWT/auth/AuthService.java
+++ src/main/java/com/example/SpringSecurityJWT/auth/AuthService.java
@@ -4,6 +4,9 @@ import com.example.SpringSecurityJWT.config.JwtService;
 import com.example.SpringSecurityJWT.user.User;
 import com.example.SpringSecurityJWT.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private  final UserRepository userRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
 
     public AuthenticationResponse register(RegisterRequest registerRequest) {
@@ -18,13 +23,38 @@ public class AuthService {
                 .firstName(registerRequest.getFirstName())
                 .lastName(registerRequest.getLastName())
                 .email(registerRequest.getEmail())
-                .password(registerRequest.getPassword())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
                 .role(registerRequest.getRole())
                 .build();
         var savedUser = userRepository.save(user);
         String jwtToken = jwtService.generateToken(user);
 
         return AuthenticationResponse.builder().accessToken(jwtToken).build();
+    }
+
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        //FirstStep
+        //We need to validate our request (validate whether password & username is correct)
+        //Verify whether user present in the database
+        //Which AuthenticationProvider -> DaoAuthenticationProvider (Inject)
+        //We need to authenticate using authenticationManager injecting this authenticationProvider
+        //SecondStep
+        //Verify whether userName and password is correct => UserNamePasswordAuthenticationToken
+        //Verify whether user present in db
+        //generateToken
+        //Return the token
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().accessToken(jwtToken).build();
+
     }
 
 
